99ebd4b78c27046b505f947984f4ebec4e5ab87f
Davi-D18 <luizdavi.code@gmail.com>
2026-04-12 19:58:12 -0300

    feat(menu): adicionar menu principal e opções interativas

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
+from cli.menus import (
+    check_availability,
+    create_enrollment,
+    menu,
+    register_new_mentoree,
+    seek_mentorship,
+    update_confirmation,
+)
 from scripts.init_db import DBInitializer
+from utils import clear_screen, show_loading, sleep
 
-print("Iniciando a criação do banco de dados...")
-db = DBInitializer()
-db.run()
+OPTIONS = {
+    1: seek_mentorship,
+    2: check_availability,
+    3: register_new_mentoree,
+    4: update_confirmation,
+    5: create_enrollment,
+}
+
+
+def main():
+    show_loading("Iniciando o sistema")
+
+    db = DBInitializer()
+
+    show_loading("Iniciando banco de dados")
+    db.run()
+
+    try:
+        while True:
+            selected_option = menu()
+            action = OPTIONS.get(selected_option)
+
+            if selected_option == 6:
+                show_loading("Saindo do sistema")
+                break
+
+            if not action:
+                print("Opção inválida.")
+                sleep(1.5)
+                clear_screen()
+                continue
+
+            action()
+
+            clear_screen()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
